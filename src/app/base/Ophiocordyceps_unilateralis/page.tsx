6b4bcbb8c83f5e7d42874a2158f72d3d2fb81d5d
fancycoder328 <fantasticcoder999@gmail.com>
2024-05-09 00:40:15 +0530

    Expanded /base/Chromosome/page.tsx's article
    Working next on rest of Ophiocordyceps_unilateralis to complete the article

--- src/app/base/Ophiocordyceps_unilateralis/page.tsx
+++ src/app/base/Ophiocordyceps_unilateralis/page.tsx
@@ -3,6 +3,7 @@ import ParagraphSecondaryHeader from "@/components/ParagraphSecondaryHeader";
 import Link from "@/components/Link";
 import Bold from "@/components/Bold";
 import ReferencingVisual from "@/components/ReferencingVisual";
+import SubTitle from "@/components/SubTitle";
 
 export default function Ophiocordyceps_unilateralis() {
     const content = (
@@ -19,7 +20,9 @@ export default function Ophiocordyceps_unilateralis() {
                 <br />
                 <br />
                 <br />
-                <h1 className="text-xl font-bold">Cordyceps vs Ophiocordyceps</h1>
+            </p>
+            <h1 className="text-xl font-bold">Cordyceps vs Ophiocordyceps</h1>
+            <p>
                 <br />
                 Throughout history there has been confusion about the distinction between the genera <Bold>Cordyceps</Bold> and <i><Bold>Ohpiocordyceps</Bold></i>. There have been many debates about whether the zombie-ant fungus (and other fungi) belonged to one or to the other as <i>Ophiocordyceps</i> was only recently brought forward.
                 <br />
@@ -31,7 +34,9 @@ export default function Ophiocordyceps_unilateralis() {
                 <br />
                 <br />
                 <br />
-                <h1 className="text-xl font-bold"><i>Ophiocordyceps unilateralissensu lato</i></h1>
+            </p>
+            <h1 className="text-xl font-bold"><i>Ophiocordyceps unilateralissensu lato</i></h1>
+            <p>
                 <br />
                 The fungus&apos;s scientific name is sometimes written as <i>Ophiocordyceps unilateralis sensu lato</i>, which means &apos;in the <Bold>broad sense</Bold>&apos;, because the species actually represents a complex of many species within <i>Ophiocordyceps unilateralis</i>.
                 <br />
@@ -46,54 +51,54 @@ export default function Ophiocordyceps_unilateralis() {
                 <br />
                 <br />
                 <br />
-                <h3 className="font-bold text-lg">Species within the O. unilateralis core clade as described in 2018:</h3>
-                <br />
-                <div className="pl-4">
-                    <ul className="list-disc pl-4">
-                        <li className="italic">O. albacongiuae</li>
-                        <li className="italic">O. blakebarnesii</li>
-                        <li className="italic">O. camponoti-atricipis</li>
-                        <li className="italic">O. camponoti-balzani</li>
-                        <li className="italic">O. camponoti-bispinosi</li>
-                        <li className="italic">O. camponoti-chartificis</li>
-                        <li className="italic">O. camponoti-femorati</li>
-                        <li className="italic">O. camponoti-floridani</li>
-                        <li className="italic">O. camponoti-hippocrepidis</li>
-                        <li className="italic">O. camponoti-indiani</li>
-                        <li className="italic">O. camponoti-leonardi</li>
-                        <li className="italic">O. camponoti-melanotic</li>
-                        <li className="italic">O. camponoti-nidulantis</li>
-                        <li className="italic">O. camponoti-novogranadensis</li>
-                        <li className="italic">O. camponoti-renggeri</li>
-                        <li className="italic">O. camponoti-saundersi</li>
-                        <li className="italic">O. halabalaensis</li>
-                        <li className="italic">O. kimflemingiae</li>
-                        <li className="italic">O. naomipierceae</li>
-                        <li className="italic">O. ootakii</li>
-                        <li className="italic">O. polyrhachis-furcata</li>
-                        <li className="italic">O. pulvinata</li>
-                        <li className="italic">O. rami</li>
-                        <li className="italic">O. satoi</li>
-                    </ul>
-                </div>
-                <br />
-                <br />
-                <h3 className="font-bold text-lg">Species within the <i>O. kniphofioides</i> subclade as described in 2018:</h3>
-                <br />
-                <div className="pl-4">
-                    <ul className="list-disc pl-4">
-                        <li className="italic">
-                            O. daceti
-                        </li>
-                        <li className="italic">
-                            O. kniphofioides
-                        </li>
-                    </ul>
-                </div>
             </p>
+            <h3 className="font-bold text-lg">Species within the O. unilateralis core clade as described in 2018:</h3>
+            <br />
+            <div className="pl-4">
+                <ul className="list-disc pl-4">
+                    <li className="italic">O. albacongiuae</li>
+                    <li className="italic">O. blakebarnesii</li>
+                    <li className="italic">O. camponoti-atricipis</li>
+                    <li className="italic">O. camponoti-balzani</li>
+                    <li className="italic">O. camponoti-bispinosi</li>
+                    <li className="italic">O. camponoti-chartificis</li>
+                    <li className="italic">O. camponoti-femorati</li>
+                    <li className="italic">O. camponoti-floridani</li>
+                    <li className="italic">O. camponoti-hippocrepidis</li>
+                    <li className="italic">O. camponoti-indiani</li>
+                    <li className="italic">O. camponoti-leonardi</li>
+                    <li className="italic">O. camponoti-melanotic</li>
+                    <li className="italic">O. camponoti-nidulantis</li>
+                    <li className="italic">O. camponoti-novogranadensis</li>
+                    <li className="italic">O. camponoti-renggeri</li>
+                    <li className="italic">O. camponoti-saundersi</li>
+                    <li className="italic">O. halabalaensis</li>
+                    <li className="italic">O. kimflemingiae</li>
+                    <li className="italic">O. naomipierceae</li>
+                    <li className="italic">O. ootakii</li>
+                    <li className="italic">O. polyrhachis-furcata</li>
+                    <li className="italic">O. pulvinata</li>
+                    <li className="italic">O. rami</li>
+                    <li className="italic">O. satoi</li>
+                </ul>
+            </div>
+            <br />
+            <br />
+            <h3 className="font-bold text-lg">Species within the <i>O. kniphofioides</i> subclade as described in 2018:</h3>
+            <br />
+            <div className="pl-4">
+                <ul className="list-disc pl-4">
+                    <li className="italic">
+                        O. daceti
+                    </li>
+                    <li className="italic">
+                        O. kniphofioides
+                    </li>
+                </ul>
+            </div>
             <ParagraphSecondaryHeader id="Morphology">Morphology</ParagraphSecondaryHeader>
+            <h1 className="text-xl font-bold">Typical morphology</h1>
             <p id="Morphology">
-                <h1 className="text-xl font-bold">Typical morphology</h1>
                 <br />
                 The zombie-ant fungus is easily identifiable when its reproductive structure becomes apparent on its dead host, usually a carpenter ant. At the end of its life cycle, <i>O. unilateralis</i> typically generates a single, wiry yet pliant, darkly pigmented stroma which arises from the dorsal pronotum region of the ant once it is dead. Moreover, perithecia, the spore-bearing sexual structure, can be observed on the stalk, just below its tip. This complex forms the fungus&apos; fruiting body.
                 <br />
@@ -101,16 +106,33 @@ export default function Ophiocordyceps_unilateralis() {
                 Most species within the <i>O. unilateralis s.l.</i> species complex have both a sexual (teleomorph) and an asexual morph (anamorph). These are different in terms of their function and characteristics. Generally, the asexual morphs identified for Ophiocordyceps are <i>Hirsutella</i> and <i>Hymenostilbe</i>, two genera of asexually reproducing fungi.
                 <br />
                 <br />
-                <ReferencingVisual src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/f4/Schematic_drawing_of_Ophiocordyceps_unilateralis_morphology_on_an_infected_ant.png/350px-Schematic_drawing_of_Ophiocordyceps_unilateralis_morphology_on_an_infected_ant.png" w={400} h={400} caption={
-                    (
-                        <>
-                            Schematic representation of Ophiocordyceps unilateralis growing out of an infected host ant
-                        </>
-                    )
-                } />
+            </p>
+            <ReferencingVisual src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/f4/Schematic_drawing_of_Ophiocordyceps_unilateralis_morphology_on_an_infected_ant.png/350px-Schematic_drawing_of_Ophiocordyceps_unilateralis_morphology_on_an_infected_ant.png" w={400} h={400} caption={
+                (
+                    <>
+                        Schematic representation of Ophiocordyceps unilateralis growing out of an infected host ant
+                    </>
+                )
+            } />
+            <br />
+            <br />
+            <SubTitle>Morphological variation</SubTitle>
+            <p>
+                <i>O. unilateralis</i> species exhibit morphological variations which are most certainly due to their wide geographic range, from Japan to the Americas. Moreover, it has been hypothesized that their morphological variations may also be a result of one fungus species maximizing its infection on one specific host ant species (host-specific infections). Different subspecies of ant can occur within the same area, which means that in order to coexist they have to occupy different ecological niches. Consequently, the fungi may have evolved at the subspecies level in order to maximize its fitness.
+            </p>
+            <br />
+            <br />
+            <SubTitle><i>O. unilateralis</i> core clade morphological characteristics</SubTitle>
+            <p>
+                The <i>O. unilateralis</i> core clade, as described in 2018, has distinct morphological characteristics. It exhibits a single stroma with a <i>Hirsutella</i> asexual morph, which arises from the dorsal neck region of the dead ant and produces a dark brown perithecia attached to its stalk. These species are also recognizable through the host species they infect, which are only Camponotini species. Once the host is killed by the fungus, it is commonly found fixed through their mandibles onto the surfaces of leaves.
                 <br />
                 <br />
             </p>
+            <SubTitle><i>O. kniphofioides</i> subclade morphological characteristics</SubTitle>
+            <p>
+                The <i>O. kniphofioides</i> subclade, as described in 2018, also has distinct morphological characteristics. Its species produce a stroma that grows laterally from the host&apos;s thorax which itself generates an orange ascoma. Moreover, species within this subclade share a <i>Hirsutella</i> asexual morph. As for the core clade, these species are also recognizable through the hosts they infect, which are usually neotropical ant species. The subclade does not present the same extended phenotype with the famous &quot;death grip&quot; that <i>O. unilateralis</i> species typically exhibit. Their hosts usually die at the base of large trees in the Amazonian rainforest, among the moss carpets.
+            </p>
+            <ParagraphSecondaryHeader id="This_can_be_continued">[This can be continued]</ParagraphSecondaryHeader>
             <ParagraphSecondaryHeader id="Scientific_classification">Scientific classification</ParagraphSecondaryHeader>
             <p id="Scientific_classification">
                 <Bold>Domain</Bold>: <Link slug="Eukaryote" title="Eukaryota" />;<br />
